84dfb34b42dcd5ca5720dd6c0c0cffcaf0ee49c0
Vangel22 <h.vangel22@gmail.com>
2024-06-17 20:32:01 +0200

    save task

---
+++ c02/index.js
@@ -0,0 +1,17 @@
+let users = [
+  { name: "Alice", age: 25 },
+  { name: "Bob", age: 30 },
+  { name: "Charlie", age: 35 },
+  { name: "David", age: 22 },
+  { name: "Eve", age: 28 },
+];
+
+// find => go vrakja prviot pogodok vo nizata
+
+// sort => sporeduva dva elementi i soodvetno gi sortira
+
+// reduce => gi sobira site vrednosti
+
+// map => ni vrakja niza
+
+// filter => filtrirame po odreden podatok
