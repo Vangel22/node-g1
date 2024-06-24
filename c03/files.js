61d78b2bf9feddc131a4ef5a78732d4813bc64cd
Vangel22 <h.vangel22@gmail.com>
2024-06-24 19:55:44 +0200

    save

--- c03/files.js
+++ c03/files.js
@@ -1,13 +1,45 @@
 const fs = require("fs");
 
-const readFile = (fileName) => {
-  return new Promise((success, fail) => {
-    fs.readFile(fileName, "utf-8", (err, data) => {
-      if (err) return fail(err);
-      return success(data);
-    });
+// const readFile = (fileName) => {
+//   // fullfiled, pending, rejected
+//   // Promise koristime za da se spravime so async povici
+//   return new Promise((success, fail) => {
+//     // fs.readFile e async funkcija
+//     fs.readFile(fileName, "utf-8", (err, data) => {
+//       if (err) return fail(err);
+//       return success(data);
+//     });
+//   });
+// };
+
+// Callback
+
+// callback -> handleReadFile
+const readFile = (callback, fileName) => {
+  fs.readFile(fileName, "utf-8", (err, data) => {
+    if (err) {
+      callback(err); // handleReadFile(err)
+      // Callback hell
+      // callback(fn(err))
+    } else {
+      callback(data); // handleReadFile(data)
+    }
   });
 };
+
+const handleReadFile = (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+};
+
+readFile(handleReadFile, "data.txt");
+
+// Callback
+// Promise
+// Async/await
 
 const writeFile = (fileName, data) => {
   // success i fail se callback funkcii
